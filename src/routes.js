ed446292551190fe372df2baad39cc1a3f9c54a2
samuelrrs <samuellrrs@gmail.com>
2020-12-03 16:39:13 -0300

    fix: Rota dos repositorios

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Main from './pages/Main/index';
-import Repositorio from './Repositorio/index';
+import Repositorio from './pages/Repositorio/index';
 
 export default function Routes () {
     return (
