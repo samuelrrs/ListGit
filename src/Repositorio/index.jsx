2cbb3308548ae65900bb83b85730340f26925246
samuelrrs <samuellrrs@gmail.com>
2020-12-03 16:39:36 -0300

    fix: Refatorando codigo

--- src/Repositorio/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Repositorio({match}) {
-    return (
-        <h1 style={{color: '#FFF'}}>
-            {decodeURIComponent(match.params.repositorio)}
-        </h1>
-    )
-}
