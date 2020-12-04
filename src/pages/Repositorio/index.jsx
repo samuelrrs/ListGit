560aad01c9895142757aa5d6b949890547f5a7bb
samuelrrs <samuellrrs@gmail.com>
2020-12-04 10:45:59 -0300

    feat: add visulizacao das Issues e feito o map

---
+++ src/pages/Repositorio/index.jsx
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import api from '../../services/api'
+import { Container, Owner, Loading, BackButton, IssuesList } from './styles';
+import { FaArrowLeft } from 'react-icons/fa'
+
+
+export default function Repositorio({ match }) {
+    const [repositorio, setRepositorio] = useState({});
+    const [issues, setIssues] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function load() {
+            const nomeRepo = decodeURIComponent(match.params.repositorio)
+
+
+            const [repositorioData, issuesData] = await Promise.all([
+                api.get(`/repos/${nomeRepo}`),
+                api.get(`/repos/${nomeRepo}/issues`, {
+                    params: {
+                        state: 'open',
+                        per_page: 5
+                    }
+                })
+            ])
+            setRepositorio(repositorioData.data)
+            setIssues(issuesData.data);
+            setLoading(false)
+
+        }
+        load()
+    }, [match.params.repositorio])
+
+    if (loading) {
+        return (
+            <Loading>
+                <h1>Carregando</h1>
+            </Loading>
+        )
+    }
+    return (
+        <Container>
+            <BackButton to="/">
+                <FaArrowLeft color="#b41313" size={30} />
+            </BackButton>
+            <Owner>
+                <img
+                    src={repositorio.owner.avatar_url}
+                    alt={repositorio.owner.login} />
+                <h1>{repositorio.name}</h1>
+                <p>{repositorio.description}</p>
+                <p>Stars : {repositorio.stargazers_count}</p>
+                <p>Language : {repositorio.language}</p>
+            </Owner>
+
+            <IssuesList>
+                {issues.map(issue => (
+                    <li key={String(issue.id)}>
+                        <img src={issue.user.avatar_url} alt={issue.user.login} />
+
+                        <div>
+                            <strong>
+                                <a href={issue.html_url}> {issue.title} </a>
+                            </strong>
+                        </div>
+
+                    </li>
+                ))}
+            </IssuesList>
+        </Container>
+    )
+}
