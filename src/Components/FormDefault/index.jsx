7c731ccd679a7bb10356c28435ff5869a5e294a7
samuelrrs <samuellrrs@gmail.com>
2020-12-08 16:46:46 -0300

    fix: Renderização do form

--- src/Components/FormDefault/index.jsx
+++ src/Components/FormDefault/index.jsx
@@ -1,12 +1,15 @@
 import { FormControl } from '@material-ui/core';
 import React, { Children } from 'react';
-
+import useStyles from './styles';
 
 export default function FormDefault({ onSubmit, error, contentForm }) {
+
+    const styles = useStyles()
 
     return (
         <>
             <FormControl
+                className={styles.container}
                 component={'form'}
                 onSubmit={onSubmit}
                 error={error}
