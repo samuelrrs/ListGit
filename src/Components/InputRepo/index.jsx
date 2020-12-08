eaf9893d9d1ba192a9b545bd83c4f0db250f2291
samuelrrs <samuellrrs@gmail.com>
2020-12-08 16:51:01 -0300

    fix: Component do input

--- src/Components/InputRepo/index.jsx
+++ src/Components/InputRepo/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import TextField  from '@material-ui/core/TextField';
-
+import useStyles from './styles';
 
 
 export default function InputRepo( {label, value, onChange, type}) {
 
+    const styles = useStyles()
     return (
         <>
             <TextField
+                className={styles.input}
                 type={type}
                 label={label}
                 variant="outlined"
