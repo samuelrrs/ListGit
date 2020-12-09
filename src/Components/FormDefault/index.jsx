60056caa73683612d17ab7b2931706ebfc685d7d
samuelrrs <samuellrrs@gmail.com>
2020-12-09 10:36:23 -0300

    fix: Formulario com Matrial UI

---
+++ src/Components/FormDefault/index.jsx
@@ -0,0 +1,21 @@
+import { FormControl } from '@material-ui/core';
+import React  from 'react';
+import useStyles from './styles';
+
+export default function FormDefault({ onSubmit, error, contentForm }) {
+
+    const styles = useStyles()
+
+    return (
+        <>
+            <FormControl
+                className={styles.container}
+                component={'form'}
+                onSubmit={onSubmit}
+                error={error}
+            >
+                {contentForm}
+            </FormControl>
+        </>
+    );
+}
