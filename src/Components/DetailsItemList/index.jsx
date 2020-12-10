dd2caa337b03b63be787fe688e9b5eb5eb1beee4
samuelrrs <samuellrrs@gmail.com>
2020-12-09 22:21:19 -0300

    feat: Add componente de DetailsItemList

---
+++ src/Components/DetailsItemList/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import useStyles from './styles';
+import Typography from '@material-ui/core/Typography'
+import ListItem from '@material-ui/core/ListItem';
+export default function ItemList({ name, desc, stars, language, avatar, alt }) {
+
+    const styles = useStyles()
+    return (
+        <ListItem button key={name} className={styles.list}>
+
+            <img src={avatar} alt={alt}/>
+            <Typography className={styles.textList}>{name}</Typography>
+            <Typography className={styles.textList}>Descrição : {desc}</Typography>
+            <Typography className={styles.textList}>Stars: {stars}</Typography>
+            <Typography className={styles.textList}>Linguagem: {language}</Typography>
+
+        </ListItem>
+    )
+
+
+}
---
+++ src/Components/DetailsItemList/styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  list: {
+    display: 'flex',
+    flexDirection: "column",
+  }, 
+}));
+
+export default useStyles;
