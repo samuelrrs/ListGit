448d3d4d34f11cd7ae017f6b21516781d2d53faa
samuelrrs <samuellrrs@gmail.com>
2020-12-13 16:21:55 -0300

    fix: Estilizado o repositorio e removendo warnings

--- src/pages/Repositorio/index.jsx
+++ src/pages/Repositorio/index.jsx
@@ -1,12 +1,13 @@
+import { Container } from '@material-ui/core';
 import React from 'react'
-import { Container} from './styles';
 import ContainerDetailsRepo from './../../Container/ContainerDetailsRepo/index';
-
+import useStyles from './styles';
 
 export default function Repositorio() {
-    
+  
+  const styles = useStyles()
     return (
-        <Container>
+        <Container className={styles.container}>
           <ContainerDetailsRepo/>
         </Container>
     )
