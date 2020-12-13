cd2517b4e2f04d0e5b5e7d64ea9035006270204f
samuelrrs <samuellrrs@gmail.com>
2020-12-13 16:21:27 -0300

    fix: Estilos em geral

--- src/Components/CardPrincipal/styles.js
+++ src/Components/CardPrincipal/styles.js
@@ -3,6 +3,10 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(() => ({
   principal: {
     maxWidth: 345,
+    display:'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
   },
 }));
 
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,105 +1,14 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 1);
-  padding: 30px;
-  margin: 80px auto;
-`;
-export const Owner = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-
-  img {
-    width: 150px;
-    border-radius: 20%;
-    margin: 20px 0;
+const useStyles = makeStyles(() => ({
+  container: {
+    maxWidth: '700px',
+    padding: '30px',
+    margin: '80px auto',
+  
   }
-  h1 {
-    font-size: 30px;
-    color: #0d2636;
-  }
-  p {
-    margin-top: 5px;
-    font-size: 14px;
-    color: #000;
-    text-align: center;
-    line-height: 1.4;
-    max-width: 400px;
-  }
-`;
+}));
 
-export const BackButton = styled(Link)`
-  border: 0;
-  outline: 0;
-  background: transparent;
-`;
 
-export const Loading = styled.div`
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-`;
 
-export const IssuesList = styled.ul`
-  margin-top: 30px;
-  padding-top: 30px;
-  border-top: 1px solid #eee;
-  list-style: none;
-
-  li {
-    & + li {
-      margin-top: 12px;
-    }
-
-    display: flex;
-    padding: 15px 10px;
-
-    img {
-      width: 37px;
-      height: 37px;
-      border-radius: 50%;
-      border: 2px solid #0d2636;
-    }
-    div {
-      flex: 1;
-      margin-left: 12px;
-
-      p {
-        margin-top: 10px;
-        font-size: 12px;
-        color: #000;
-      }
-    }
-
-    strong {
-      font-size: 15px;
-
-      a {
-        text-decoration: none;
-        color: #222;
-
-        &:hover {
-          color: #0071db;
-        }
-      }
-
-      span {
-        background: #222;
-        color: #fff;
-        border-radius: 4px;
-        font-size: 12px;
-        font-weight: 600;
-        padding: 5px 7px;
-        margin-left: 10px;
-      }
-    }
-  }
-`;
+export default useStyles;
