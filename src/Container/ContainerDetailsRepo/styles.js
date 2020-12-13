4ea04b9cd20697745db669cd2468e33c361a2cf2
samuelrrs <samuellrrs@gmail.com>
2020-12-13 18:04:21 -0300

    fix: estilo do containerDetails e repositorio

--- src/Container/ContainerDetailsRepo/styles.js
+++ src/Container/ContainerDetailsRepo/styles.js
@@ -1,33 +1,15 @@
-import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-export const Container = styled.div``;
-
-
-export const BackButton = styled(Link)`
-  /*   border: 0;
-  outline: 0;
-  background: transparent; */
-`;
-
-export const Loading = styled.div`
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  height: 100vh;
-`;
 
 const useStyles = makeStyles((theme) => ({
- 
   cardPrincipal: {
-    display:'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection : 'column'
-  }
- 
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  tituloCard: {
+    fontWeight: "bold",
+    fontSize: "15px",
+  },
 }));
 
 export default useStyles;
--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -2,13 +2,14 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   container: {
-    maxWidth: '700px',
-    padding: '30px',
-    margin: '80px auto',
-  
-  }
+    maxWidth: "700px",
+    padding: "30px",
+    marginTop: "25px",
+    backgroundColor: "#fff",
+    borderRadius: "25px",
+    opacity: '1',
+
+  },
 }));
-
-
 
 export default useStyles;
