76570025100a1a6032c70a55c69f3fe24e245c85
samuelrrs <samuellrrs@gmail.com>
2020-12-13 22:37:27 -0300

    feat: Criação do componente exibindo dica de pesquisa

---
+++ src/Components/TipMessage/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import useStyles from './styles';
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export default function CustomizedSnackbars() {
+  const [open, setOpen] = React.useState(true);
+
+ 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const styles = useStyles()
+
+  return (
+      <Snackbar open={open} autoHideDuration={26000} onClose={handleClose} className={styles.tip}>
+        <Alert onClose={handleClose} severity="info">
+          Dica : facebook/react-native
+        </Alert>
+      </Snackbar>
+  );
+}
---
+++ src/Components/TipMessage/styles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  tip: {
+     marginBottom: '670px',
+  }
+}));
+
+export default useStyles;
