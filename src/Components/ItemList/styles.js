cf07f43af8eae2d3ed166df4e5283b09c5b58f95
samuelrrs <samuellrrs@gmail.com>
2020-12-15 16:45:18 -0300

    fix: Removido estilos que não estavam mais sendo usados

--- src/Components/ItemList/styles.js
+++ src/Components/ItemList/styles.js
@@ -2,13 +2,29 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   list: {
-    display: 'flex',
+    display: "flex",
     flexDirection: "row",
-    justifyContent: 'space-between',
-  }, 
-  textList: {
-   marginRight: '400px'
-  }
+    justifyContent: "space-between",
+    "&:visited": {
+      color: "#9370DB",
+    },
+  },
+  iconDelete: {
+    width: "10%",
+    display: "flex",
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+
+  },
+  repoName: {
+    width: "100%",
+  },
+
+  iconDetails: {
+    color: "#8A2BE2",
+    width: '5%',
+
+  },
 }));
 
 export default useStyles;
--- src/Components/TipMessage/styles.js
+++
@@ -1,8 +0,0 @@
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  tip: {
-  }
-}));
-
-export default useStyles;
